914592ef03b85686321c0e5e0aff50a089ce1a51
GZhiDao1010 <GZhiDao1010@gmail.com>
2017-02-07 23:18:44 +0800

    java

---
+++ src/db/Uitls.java
@@ -0,0 +1,15 @@
+package db;
+
+/**
+ * @author gzhidao   alt + shift + j
+ *
+ */
+public class Uitls {
+	
+	//主函数、主方法保证类的独立运行
+	public static void main(String[] args)
+	{
+		System.out.print("3123131");
+		System.out.print("3123131");
+	}
+}
