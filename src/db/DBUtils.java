2c97195b441b8d906373fd685b6492787cf1a3f2
GZhiDao1010 <GZhiDao1010@gmail.com>
2017-02-11 23:57:24 +0800

    数据库链接

--- src/db/DBUtils.java
+++ src/db/DBUtils.java
@@ -1,10 +1,37 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author GZhi·Dao
  * @time 2017年2月11日 下午11:42:24
  * @functions 链接数据库
  */
 public class DBUtils {
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/imooc?useUnicode=true&amp;characterEncoding=utf-8";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
+	
+	private static Connection conn = null;
+	
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(URL,USER,PASSWORD);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public static Connection getConn() {
+		return conn;
+	}
+
+	public static void setConn(Connection conn) {
+		DBUtils.conn = conn;
+	}
 }
