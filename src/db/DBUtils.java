bd6c6b9fc7bb95245b274fde160e8c24106f401f
GZhiDao1010 <GZhiDao1010@gmail.com>
2017-02-12 00:21:30 +0800

    链接数据库

--- src/db/DBUtils.java
+++ src/db/DBUtils.java
@@ -2,7 +2,9 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author GZhi·Dao
@@ -13,25 +15,53 @@ public class DBUtils {
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/imooc?useUnicode=true&amp;characterEncoding=utf-8";
 	private static final String USER = "root";
 	private static final String PASSWORD = "root";
+	//private static Connection conn;
 	
-	private static Connection conn = null;
+//	private static Connection conn = null;
+//	
+//	static {
+//		try {
+//			//1.加载驱动程序
+//			Class.forName("com.mysql.jdbc.Driver");
+//			//2.获得数据库链接
+//			conn = DriverManager.getConnection(URL,USER,PASSWORD);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	public static Connection getConn() {
+//		return conn;
+//	}
+//
+//	public static void setConn(Connection conn) {
+//		DBUtils.conn = conn;
+//	}
 	
-	static {
+	@SuppressWarnings("unused")
+	public static void main(String[] args)
+	{
 		try {
+			//1.加载驱动程序
 			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(URL,USER,PASSWORD);
-		} catch (ClassNotFoundException e) {
+			//2.获得数据库链接
+			Connection conn = DriverManager.getConnection(URL,USER,PASSWORD);
+			//3.通过数据库的链接操作数据库，实现增删改查
+			Statement stmt = conn.createStatement();
+			//4.获取数据库中的指定信息
+			ResultSet rs = stmt.executeQuery("select user_name,age from imooc_goddess");
+			
+			while(rs.next()){ //5.有数据循环打印...
+				System.out.println("姓名："+ rs.getString("user_name") + ",年龄："+rs.getInt("age"));
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}catch (SQLException e) {
-			e.printStackTrace();
+		}catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
 		}
 	}
-
-	public static Connection getConn() {
-		return conn;
-	}
-
-	public static void setConn(Connection conn) {
-		DBUtils.conn = conn;
-	}
+	
+	
 }
